7aff1d1888dcdda46af761f5b8722b25e165b661
Michael Allan Jackson <m@j5n.dev>
2024-09-04 19:50:14 -0400

    Clean up wrtiing messages

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,17 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const stdout = std.io.getStdOut().writer();
+    const stderr = std.io.getStdErr().writer();
     if (std.os.argv.len < 3) {
-        try stdout.print("Error: A command and an argument are needed.\n", .{});
-        try bw.flush();
+        try stderr.print("A command and an argument are needed.\n", .{});
         std.process.exit(1);
     }
     const command = std.os.argv[1];
     if (!std.mem.eql(u8, std.mem.span(command), "clone")) {
-        try stdout.print("Error: command '{s}' unknown.\n", .{command});
-        try bw.flush();
+        try stderr.print("The command '{s}' is unknown.\n", .{command});
         std.process.exit(1);
     }
-
     const argument = std.os.argv[2];
     try stdout.print("Argument: {s}\n", .{argument});
-    try bw.flush();
 }
