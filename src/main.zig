3795aae429cafab5b9735156f879bb11f3a9b1e7
Michael Allan Jackson <m@j5n.dev>
2024-09-05 10:50:03 -0400

    Use std.process.argsAlloc instead of std.os.argv

--- src/main.zig
+++ src/main.zig
@@ -3,15 +3,20 @@ const std = @import("std");
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     const stderr = std.io.getStdErr().writer();
-    if (std.os.argv.len < 3) {
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const args = try std.process.argsAlloc(allocator);
+    if (args.len < 3) {
         try stderr.print("A command and an argument are needed.\n", .{});
         std.process.exit(1);
     }
-    const command = std.os.argv[1];
-    if (!std.mem.eql(u8, std.mem.span(command), "clone")) {
+    const command = args[1];
+    if (!std.mem.eql(u8, command, "clone")) {
         try stderr.print("The command '{s}' is unknown.\n", .{command});
         std.process.exit(1);
     }
-    const argument = std.os.argv[2];
+    const argument = args[2];
     try stdout.print("Argument: {s}\n", .{argument});
 }
