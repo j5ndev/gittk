75a155af82f1a399c47e06d22ac40888941202ce
Michael Allan Jackson <m@j5n.dev>
2025-08-08 23:19:45 -0400

    Work in progress on list command

--- src/commands/list.zig
+++ src/commands/list.zig
@@ -1,13 +1,37 @@
 const std = @import("std");
 
-pub const ListError = error{
-    NotImplemented,
+pub const ListError = error{};
+
+const Entry = struct {
+    name: []const u8,
+    kind: std.fs.Dir.Entry.Kind,
 };
 
 // Execute the list command
 pub fn execute(projectDir: []const u8, allocator: std.mem.Allocator) ListError!void {
-    _ = projectDir;
-    _ = allocator;
+    var dir = try std.fs.cwd().openDir(projectDir, .{ .iterate = true });
+    defer dir.close(); 
 
-    return ListError.NotImplemented;
+    var entries = std.ArrayList(Entry).init(allocator);
+    defer {
+        for (entries.items) |entry| {
+            allocator.free(entry.name);
+        }
+        entries.deinit();
+    }
+    for (entries.items) |entry| {
+        const full_len = projectDir.len + 1 + entry.name.len;
+        var path_buf = try allocator.alloc(u8, full_len);
+        defer allocator.free(path_buf);
+
+        std.mem.copyForwards(u8, path_buf[0..projectDir.len], projectDir);
+        path_buf[projectDir.len] = '/';
+        std.mem.copyForwards(u8, path_buf[projectDir.len + 1 ..], entry.name);
+        const sub_path = path_buf[0..full_len];
+        if (entry.kind == .directory) {
+            try execute (sub_path, allocator);
+        } else {
+            std.debug.print("{s}\n", sub_path);
+        }
+    }
 }
--- src/commands/tree.zig
+++ src/commands/tree.zig
@@ -7,6 +7,7 @@ pub const TreeError = error{
 
 // Execute the tree command
 // TODO: Don't require tree on PATH
+//       See https://gist.github.com/j5ndev/048153ede373f68b9d4d7ff3951f0e03
 pub fn execute(projectDir: []const u8, allocator: std.mem.Allocator) TreeError!void {
     const argv = [_][]const u8{ "tree", projectDir, "-L", "3"};
     var proc = std.process.Child.init(&argv, allocator);
